ea8ddf6f22a69c0cc171f2ef223842cea8138c56
John Katua <johnkatua99@gmail.com>
2025-05-31 09:59:36 +0300

    feat: add a logging function to the application

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+
+def setup_logger(config):
+    config_logger = config['logging']
+    log_file = config_logger['log_file']
+    level = config_logger['level'].upper()
+    log_level = getattr(logging, level, logging.INFO)
+
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    logging.basicConfig(
+        filename=log_file,
+        level=log_level,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
+    return logging.getLogger()
