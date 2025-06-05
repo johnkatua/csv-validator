9125f77c4b9de6f5531211d06a505485bb3e5ebb
John Katua <johnkatua99@gmail.com>
2025-06-05 20:24:50 +0300

    feat: add an index for the dataframes

--- src/csv_handler/load.py
+++ src/csv_handler/load.py
@@ -8,22 +8,14 @@ def read_csvs(config):
     try:
         source_path = config['source_file']
         target_path = config['target_file']
-        key_field = config['primary_key']
+        
         df_source = pd.read_csv(source_path)
         df_target = pd.read_csv(target_path)
 
         # Rename columns to match
         rename_columns(df_source, config)
 
-        # Set index
-        # df_source = df_source.set_index(key_field)
-        # df_target = df_target.set_index(key_field)
-
-        # Filter common columns
-        # transform_df(df_source, df_target, config)
         return df_source, df_target
-
-        # print(df_source, df_target)
         
     except Exception as e:
         logger.error(f"Error comparing CSVs: {e}")
--- src/csv_handler/transform.py
+++ src/csv_handler/transform.py
@@ -2,9 +2,14 @@ from src.csv_handler.load import read_csvs
 
 def transform_df(config):
     compare_cols = config['compare_columns']
+    key_field = config['primary_key']
     df_target, df_source = read_csvs(config)
     cols_in_both = [col for col in compare_cols if col in df_source.columns and col in df_target.columns]
     df_source = df_source[cols_in_both]
     df_target = df_target[cols_in_both]
+
+    # Set index
+    df_source = df_source.set_index(key_field)
+    df_target = df_target.set_index(key_field)
 
     print(df_source, df_target)
